04b5162f1d99c232660b9860ba58a0b441680300
Dusty Mabe <dusty@dustymabe.com>
2016-02-16 22:52:36 -0500

    blah

---
+++ lib/vagrant-dusty-sshfs.rb
@@ -0,0 +1,17 @@
+begin
+  require "vagrant"
+rescue LoadError
+  raise "The Vagrant sshfs plugin must be run within Vagrant"
+end
+
+require "vagrant-dusty-sshfs/version"
+require "vagrant-dusty-sshfs/plugin"
+
+module VagrantPlugins
+  module SyncedFolderSSHFS
+    # Returns the path to the source of this plugin
+    def self.source_root
+      @source_root ||= Pathname.new(File.expand_path('../../', __FILE__))
+    end
+  end
+end
